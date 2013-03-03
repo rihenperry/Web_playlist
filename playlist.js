ca99e623a3e3690c4b2c70d63cfad4bcd472ddb0
rihen pereira <rihen234@gmail.com>
2013-03-04 01:28:35 +0530

    added init  function for loading event handler function "add song"

--- playlist.js
+++ playlist.js
@@ -1,4 +1,8 @@
+function init() {
+    document.getElementById("push_song").onclick = add_song;
+}
+
 var add_song = function () {
     // this function loads the text data into playlist box
     alert("button is clicked");
-}
+};
